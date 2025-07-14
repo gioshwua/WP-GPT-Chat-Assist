081be6667919ed16cf2163b37f900f3a0ffbb1c0
Kenneth Joshua Reaño <devken@Kenneths-Macbook-Pro.local>
2025-07-14 18:43:26 +0800

    docs: added plugin description

---
+++ ai-shortcode-chat.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WP-PromptBridge-AI
+ * Plugin URI:https://github.com/gioshwua/WP-PromptBridge-AI
+ * Description: Adds an AI chat box to your WordPress site using a shortcode and secure backend API calls. Supports OpenAI, Hugging Face, and OpenRouter.
+ * Version: 1.1
+ * Author: Kenneth Joshua Reaño
+ * Author URI: https://github.com/gioshwua
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ * Text Domain: ai-shortcode-chat
+ * Domain Path: /languages
+ */
+
